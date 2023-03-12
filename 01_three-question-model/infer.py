156aebe074bf9c01637c7200ec1e6ba2bd35c180
karthik-d <karthikdesingu2000@gmail.com>
2023-03-13 00:34:13 +0530

    modeled three and four q models

---
+++ 01_three-question-model/infer.py
@@ -0,0 +1,63 @@
+import os
+import pystan
+import pickle
+#from matplotlib import pyplot as plot
+
+MODEL_PATH = os.getcwd()
+MODEL_SRCNAME = 'model.stan'
+MODEL_FILENAME = 'model.pkl'
+
+# GENERATE ALL COMBINATION OF SCORES
+
+data_dict = ('isCorrect1', 'isCorrect2', 'isCorrect3')
+allowed_values = (0, 1)
+observed_data = [dict(zip(data_dict,
+                        [(a,b,c) for a in allowed_values for b in allowed_values for c in allowed_values][x])) for x in range(len(allowed_values)**len(data_dict))]
+
+
+#observed_data = [{'isCorrect1':1, 'isCorrect2':0, 'isCorrect3':1}]
+
+#Compilation of model
+try:
+    model_file_path = os.path.join(MODEL_PATH, MODEL_FILENAME)
+    model_file = open(model_file_path, 'rb')
+except FileNotFoundError:
+    model_source_path = os.path.join(MODEL_PATH, MODEL_SRCNAME)
+    with open(model_source_path) as model_source:
+        model_code = model_source.read()
+    model = pystan.StanModel(model_code=model_code)
+
+    # Save the compiled model to avoid recompilation
+    # Explicitly delete the *.pkl file if model has been modified
+    with open(model_file_path, 'wb') as model_file:
+        pickle.dump(model, model_file)
+else:
+    model = pickle.load(model_file)
+    model_file.close()
+
+inference = {'skill1':list(), 'skill2':list()}
+for data in observed_data:
+    result = model.sampling(data=data)
+    print(result)
+    summary = result.summary(pars=('P_skill1', 'P_skill2'))
+    inference['skill1'].append(summary['summary'][0][0])
+    inference['skill2'].append(summary['summary'][1][0])
+
+    """plot.hist(result['skill1'], bins=[x*0.01 for x in range(0,101)], rwidth=0.9)
+    plot.title('Probability Distribution')
+    plot.show()
+
+    plot.hist(result['skill2'], bins=[x*0.01 for x in range(0,101)], rwidth=0.9)
+    plot.title('Probability Distribution')
+    plot.show()
+
+    plot.hist(result['hasSkills'], bins=[x*0.01 for x in range(0,101)], rwidth=0.9)
+    plot.title('Probability Distribution')
+    plot.show()"""
+
+print()
+for k in observed_data:
+    print(k)
+print("\nSKILL 1".ljust(24), "SKILL 2".ljust(24))
+for i in range(len(inference['skill1'])):
+    print(str(inference['skill1'][i]).ljust(24), str(inference['skill2'][i]).ljust(24))
---
+++ 01_three-question-model/model.stan
@@ -0,0 +1,50 @@
+data{
+    int<lower=0,upper=1> isCorrect1;
+    int<lower=0,upper=1> isCorrect2;
+    int<lower=0,upper=1> isCorrect3;
+}
+
+parameters{
+    real<lower=0,upper=1> skill1;
+    real<lower=0,upper=1> skill2;
+}
+
+transformed parameters{
+    vector[4] likelihoods;
+    real<lower=0,upper=1> p_knows;
+    real<lower=0,upper=1> p_guesses;
+
+    p_knows = 0.9;
+    p_guesses = 0.2;
+    likelihoods[1] = bernoulli_lpmf(isCorrect1|p_knows)+bernoulli_lpmf(isCorrect2|p_knows)+bernoulli_lpmf(isCorrect3|p_knows);
+    likelihoods[2] = bernoulli_lpmf(isCorrect1|p_knows)+bernoulli_lpmf(isCorrect2|p_guesses)+bernoulli_lpmf(isCorrect3|p_guesses);
+    likelihoods[3] = bernoulli_lpmf(isCorrect1|p_guesses)+bernoulli_lpmf(isCorrect2|p_knows)+bernoulli_lpmf(isCorrect3|p_guesses);
+    likelihoods[4] = bernoulli_lpmf(isCorrect1|p_guesses)+bernoulli_lpmf(isCorrect2|p_guesses)+bernoulli_lpmf(isCorrect3|p_guesses);
+}
+
+model{
+    skill1 ~ beta(2,2);   // P(s1)
+    skill2 ~ beta(2,2);   // P(s2)
+
+    target += log(skill1) + log(skill2) + likelihoods[1];
+    target += log(skill1) + log(1-skill2) + likelihoods[2];
+    target += log(1-skill1) + log(skill2) + likelihoods[3];
+    target += log(1-skill1) + log(1-skill2) + likelihoods[4];
+}
+
+generated quantities{
+    real P_skill1;
+    real P_skill2;
+
+    P_skill1 = log_sum_exp(log(skill1)+log(skill2)+likelihoods[1], log(skill1)+log(1-skill2)+likelihoods[2]);
+    print(P_skill1);
+    print(log_sum_exp(P_skill1, log_sum_exp(log(1-skill1)+log(skill2)+likelihoods[3], log(1-skill1)+log(1-skill2)+likelihoods[4])));
+    P_skill1 -= log_sum_exp(P_skill1, log_sum_exp(log(1-skill1)+log(skill2)+likelihoods[3], log(1-skill1)+log(1-skill2)+likelihoods[4]));
+    P_skill1 = exp(P_skill1);
+
+    P_skill2 = log_sum_exp(log(skill1)+log(skill2)+likelihoods[1], log(1-skill1)+log(skill2)+likelihoods[3]);
+    print(P_skill2);
+    print(log_sum_exp(P_skill2, log_sum_exp(log(skill1)+log(1-skill2)+likelihoods[2], log(1-skill1)+log(1-skill2)+likelihoods[4])));
+    P_skill2 -= log_sum_exp(P_skill2, log_sum_exp(log(skill1)+log(1-skill2)+likelihoods[2], log(1-skill1)+log(1-skill2)+likelihoods[4]));
+    P_skill2 = exp(P_skill2);
+}
